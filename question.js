a178dc178b0309ed9e2f9fbb386e65c41e7a8f31
divyang bagla <57760543+bagladivyang03@users.noreply.github.com>
2020-04-04 17:13:39 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,72 @@
+var questions =[{
+  "question" : "Who was the first Indian woman in Space?",
+  "option1" : "Kalpana Chawla",
+  "option2" : "Sunita Williams",
+  "option3" : "Koneru Humpy",
+  "option4" : "None of the above",
+  "answer" : "3"
+},{
+  "question" : "Who was the first Indian in space?",
+  "option1" : "Vikram Ambalal",
+  "option2" : "Ravish Malhotra",
+  "option3" : "Rakesh Sharma",
+  "option4" : "Nagapathi Bhat",
+  "answer" : "3"
+},{
+  "question" : "Which of the following is the capita of arunachal pradesh ?",
+  "option1" : "Itanagar",
+  "option2" : "Dispur",
+  "option3" : "Imphal",
+  "option4" : "Panaji",
+  "answer" : "1"
+},{
+  "question" : "India is a federal union comprising twenty-nine states and how many union territories?",
+  "option1" : "6",
+  "option2" : "7",
+  "option3" : "8",
+  "option4" : "9",
+  "answer" : "2"
+},{
+  "question" : "What are major languages spoken in Andhra Pradesh ?",
+  "option1" : "English and Telugu",
+  "option2" : "Telugu and Urdu",
+  "option3" : "Telugu and Kannada",
+  "option4" : "All of the above languages",
+  "answer" : "2"
+},{
+  "question" : "What is the state flower of Haryana ?",
+  "option1" : "Lotus",
+  "option2" : "Rhododendron",
+  "option3" : "Golden shower",
+  "option4" : "Not declared",
+  "answer" : "1"
+},{
+  "question" : "Which of the following states is not located in the North ??",
+  "option1" : "Jharkhand",
+  "option2" : "Jammu and Kashmir",
+  "option3" : "Himachal Pradesh",
+  "option4" : "Haryana",
+  "answer" : "1"
+},{
+  "question" : "In which state the main language is the khasi ?",
+  "option1" : "Mizoram",
+  "option2" : "Nagaland",
+  "option3" : "Meghalaya",
+  "option4" : "Tripura",
+  "answer" : "3"
+},{
+  "question" : "Which state is the largest coffee producing state of India ?",
+  "option1" : "Kerala",
+  "option2" : "Tamil Nadu",
+  "option3" : "Karnataka",
+  "option4" : "Arunachal Pradesh",
+  "answer" : "3"
+},{
+  "question" : "Which state has the largest area ?",
+  "option1" : "Maharashtra",
+  "option2" : "MAdhya Pradesh",
+  "option3" : "Uttar Pradesh",
+  "option4" : "Rajasthan",
+  "answer" : "4"
+}
+];
